6fa00ad36d1ca0b734eb591ec75212481bf7c68a
unknown <Oscar@325-1.iab.kz>
2023-10-27 12:05:58 +0600

    chore(project): Cofigure simple chat example.

--- scr/app.ts
+++ scr/app.ts
@@ -1,1 +1,11 @@
-console.info("hello")
+const { Telegraf } = require ('Telegraf')
+const bot = new Telegraf("6725226089:AAE_p3oh9YG3aedoaaP6l3lUDqpYHkCm_wQ")
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hello !!!!!!!'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
